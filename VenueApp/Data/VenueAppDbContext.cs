dbac119b33fa49b45f9ac056d09963002ccae351
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2019-01-26 19:02:22 -0500

    Added Initial Models and Controllers

---
+++ VenueApp/Data/VenueAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VenueApp.Models;
+
+namespace VenueApp.Data
+{
+    public class VenueAppDbContext : DbContext
+    {
+
+        public VenueAppDbContext(DbContextOptions<VenueAppDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<EventCategory> Categories { get; set; }
+
+    }
+}
---
+++ VenueApp/Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VenueApp.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public double Price { get; set; }
+        public EventCategory Category { get; set; }
+        public int CategoryID { get; set; }        
+        
+        //public DateTime Created { get; set; }     
+
+    }
+}
