6447807c35d299802015b25302c2afd6d32f69de
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2019-02-05 00:37:45 -0500

    Create the Routes and the simple initial Views for both Dashboards. Beggin experimenting with routes and app flow.

---
+++ VenueApp/Controllers/DashboardController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+
+
+namespace VenueApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (HttpContext.Session.TryGetValue("User", out byte[] value))
+            {
+                string userType = HttpContext.Session.GetString("Type");
+
+                if (userType == "user")
+                {
+                    return Redirect("/UserDashboard");
+                }
+                else if (userType == "admin")
+                {
+                    return Redirect("/AdminDashboard");
+                }
+            }
+
+            //Error go Back to Login
+            return RedirectToAction("Login", "User");
+        }
+
+        [Route("UserDashboard")]
+        public IActionResult UserDashboard()
+        {
+            return View("User");
+        }
+
+        [Route("AdminDashboard")]
+        public IActionResult AdminDashboard()
+        {
+            return View("Admin");
+        }
+    }
+}
