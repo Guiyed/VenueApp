bcdea89f5609a617c1df910c130a37fcbc1c4119
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2019-02-23 00:51:53 -0500

    Completed Upload Profile Picture Feature

--- VenueApp/Controllers/TestController.cs
+++ VenueApp/Controllers/TestController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -7,6 +9,15 @@ namespace VenueApp.Controllers
 {
     public class TestController : Controller
     {
+        private readonly IHostingEnvironment he;
+
+        public TestController(IHostingEnvironment e)
+        {
+            he = e;
+        }
+        
+        
+        
         // GET: /<controller>/
         public IActionResult Index()
         {
@@ -31,6 +42,31 @@ namespace VenueApp.Controllers
 
             return this.Json(new { success = true });
         }
+
+
+        [HttpPost]
+        public ActionResult UploadProfilePic(IFormFile image)
+        {
+
+            if (image != null)
+            {
+                
+                //var fileName = Path.Combine(he.WebRootPath + "\\images\\",  Path.GetFileName(image.FileName));
+                var fileName = Path.Combine(he.WebRootPath + "\\images\\", HttpContext.Session.GetString("User") + Path.GetExtension(image.FileName));
+                FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate);
+                image.CopyTo(fs);
+                //image.CopyTo(new FileStream(fileName, FileMode.OpenOrCreate));
+                //image.CopyTo(new FileStream(fileName, FileMode.Create));
+                HttpContext.Session.SetString("ProfilePic", "/images/"+ HttpContext.Session.GetString("User") + Path.GetExtension(image.FileName));
+                fs.Close();
+            }                       
+            //Update USER database
+
+            return RedirectToAction("Profile", "User", new { userId = HttpContext.Session.GetInt32("UserID")});
+        }
+
+
+
 
     }
 }
