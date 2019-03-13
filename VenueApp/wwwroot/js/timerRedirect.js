454418fe395a0a7a4f77f3f034acb640b67747e0
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2019-03-13 12:08:31 -0400

    JS fixes

--- VenueApp/wwwroot/js/timerRedirect.js
+++ VenueApp/wwwroot/js/timerRedirect.js
@@ -1,22 +1,26 @@
 ﻿'use strict'; // This is not neccesary but it makes the Javascript more reliable
 
 // --- Everything here runs Before the document loads ---
-var dt = endDate;
-console.log("Viewbag " + dt);
-var end = new Date(dt);
-console.log("End " + end);
-var _second = 1000;
-var _minute = _second * 60;
-var _hour = _minute * 60;
-var _day = _hour * 24;
-var timer;
+$(document).ready(function () {
+    console.log("ready!");
+    var dt = endDate;
+    console.log("Viewbag " + dt);
+    var end = new Date(dt);
+    console.log("End " + end);
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+    timer = setInterval(showRemaining, 999);   // 0.99 second interval
+});
 
 function showRemaining() {
     var now = new Date();
     console.log("Now " + now);
     var distance = end - now;
     console.log(distance)
-    if (distance <= 0) {
+    if (distance < 0) {
         document.getElementById('timer').setAttribute("class", "text-center text-danger");
         document.getElementById('countdown').innerHTML = "!!! Redirecting !!!";
         clearInterval(timer);
@@ -39,4 +43,3 @@ function showRemaining() {
     document.getElementById('countdown').innerHTML = seconds + 'secs';
 }
 
-timer = setInterval(showRemaining, 999);   // 0.99 second interval
