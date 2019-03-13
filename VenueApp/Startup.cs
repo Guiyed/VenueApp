2caa2611fbb38cbd07ce8d0e6a39e3087c5d8d85
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2019-03-13 10:26:14 -0400

    Custom Environment Startup for Database Differentiation

--- VenueApp/Startup.cs
+++ VenueApp/Startup.cs
@@ -17,12 +17,31 @@ namespace VenueApp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        /*
+         * public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        */
+
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+
+            var environment = Configuration["ApplicationSettings:Environment"];
+        }
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
