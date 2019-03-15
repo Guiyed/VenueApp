69667a08515ce3d497b365d21d4b2a9a7a6fa13e
Guillermo Zerpa <guillermo.zerpa@hotmail.com>
2019-03-15 13:24:38 -0400

    Fixed Redirect API and Maps API on Brochure

---
+++ VenueApp/wwwroot/js/LoadMapAPI.js
@@ -0,0 +1,48 @@
+﻿'use strict';
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+
+//Tuve que poner este codigo en el HEADER para que funcionanra los mapas bien.
+var map;
+
+function loadMap() {
+    if (!map) {
+        var mapScriptUrl = 'https://www.bing.com/api/maps/mapcontrol?callback=GetMap2&key=Ar6GSgDklc17CZg1iXfmAutlA2Kru2EpLP0NFvJmllNtv3QX2VTgP3YBSY2AVVUu';
+        var script = document.createElement("script");
+        script.setAttribute('defer', '');
+        script.setAttribute('async', '');
+        script.setAttribute("type", "text/javascript");
+        script.setAttribute("src", mapScriptUrl);
+        document.body.appendChild(script);
+    }
+    console.log("Loaded MAP API");
+}
+
+function GetMap() {
+    map = new Microsoft.Maps.Map('#myMap', {});
+}
+
+function GetMap2() {
+    var loc = eventLocation;
+    map = new Microsoft.Maps.Map(document.getElementById('myMap'), {
+    //center: new Microsoft.Maps.Location(47.624527, -122.355255),
+    //mapTypeId: Microsoft.Maps.MapTypeId.aerial,
+    zoom: 50
+    });
+
+    Microsoft.Maps.loadModule('Microsoft.Maps.Search', function () {
+        var searchManager = new Microsoft.Maps.Search.SearchManager(map);
+        var requestOptions = {
+            bounds: map.getBounds(),
+            where: loc,
+            callback: function (answer, userData) {
+                map.setView({ bounds: answer.results[0].bestView });
+                map.entities.push(new Microsoft.Maps.Pushpin(answer.results[0].location, { color: 'teal' }));
+            }
+        };
+        searchManager.geocode(requestOptions);
+    });
+
+
+}
--- VenueApp/wwwroot/js/timerRedirect.js
+++ VenueApp/wwwroot/js/timerRedirect.js
@@ -2,44 +2,52 @@
 
 // --- Everything here runs Before the document loads ---
 $(document).ready(function () {
-    console.log("ready!");
-    var dt = endDate;
-    console.log("Viewbag " + dt);
-    var end = new Date(dt);
-    console.log("End " + end);
+    console.log("Timer Redirect ready!");
+    console.log("Viewbag endDate " + endDate);
+    console.log("Viewbag nowDate " + nowDate);
+    var end = new Date(endDate);
+    //console.log("End Date" + end);
+    var now = new Date(nowDate);
+    //var now = new Date();
+    //console.log("Now Date" + now);
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
     var _day = _hour * 24;
     var timer;
+    var milisecondsTicks = 1000;
 
-function showRemaining(ended) {
-    var now = new Date();
-    console.log("Now " + now);
-    var distance = ended - now;
-    console.log(distance)
-    if (distance <= 0) {
-        document.getElementById('timer').setAttribute("class", "text-center text-danger");
-        document.getElementById('countdown').innerHTML = "!!! Redirecting !!!";
-        clearInterval(timer);
-        window.location.href = '/User/Login'; //Redirect to Login page
 
-        return;
+    function showRemaining() {
+        //now = new Date();
+        var val = now.getTime();
+        val += milisecondsTicks;
+        now = new Date(val);
+        var distance = end - now;
+        console.log("Show Remaining " + distance)
+        if (distance <= 0) {
+            document.getElementById('timer').setAttribute("class", "text-center text-danger");
+            document.getElementById('countdown').innerHTML = "!!! Redirecting !!!";
+            clearInterval(timer);
+            window.location.href = '/User/Login'; //Redirect to Login page
+
+            return;
+        }
+
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        /*
+         * document.getElementById('countdown').innerHTML = hours + 'hrs ';
+        document.getElementById('countdown').innerHTML += minutes + 'mins ';
+        document.getElementById('countdown').innerHTML += seconds + 'secs';
+        */
+
+        document.getElementById('countdown').innerHTML = seconds + 'secs';
     }
 
-    var days = Math.floor(distance / _day);
-    var hours = Math.floor((distance % _day) / _hour);
-    var minutes = Math.floor((distance % _hour) / _minute);
-    var seconds = Math.floor((distance % _minute) / _second);
-
-    /*
-     * document.getElementById('countdown').innerHTML = hours + 'hrs ';
-    document.getElementById('countdown').innerHTML += minutes + 'mins ';
-    document.getElementById('countdown').innerHTML += seconds + 'secs';
-    */
-
-    document.getElementById('countdown').innerHTML = seconds + 'secs';
-}
-    timer = setInterval(showRemaining(end), 999);   // 0.99 second interval
-
+    timer = setInterval(showRemaining, milisecondsTicks);   // 0.99 second interval
+    //timer = setInterval(showRemaining, 999);   // 0.99 second interval
 });
